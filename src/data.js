aaba4716dff0d789b55c54cb1d8344a49f83bd71
hazecraft <bhavyang.mehta@outlook.com>
2026-05-15 16:45:46 -0400

    color change

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ import { FaHome, FaGraduationCap, FaChartLine, FaShieldAlt } from 'react-icons/f
 export const templateData = {
   header: {
     logoName: "Chirag Patel",
-    logoImage: "/InsureWithChiragv3-cropped.svg",
+    logoImage: "/InsureWithChiragv4-cropped.svg",
     navItems: [
       { id: "home", label: "Home" },
       { id: "services", label: "Services" },
