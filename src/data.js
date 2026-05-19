a5f7f2c02aa7242b1544b16013144d59e15263da
hazecraft <bhavyang.mehta@outlook.com>
2026-05-19 10:57:05 -0400

    tran logo png

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ import { FaHome, FaGraduationCap, FaChartLine, FaShieldAlt, FaStar, FaQuoteLeft,
 export const templateData = {
   header: {
     logoName: "Chirag Patel",
-    logoImage: "/InsureWithChirag-transparent.svg",
+    logoImage: "/InsuteWithChirag-Transparent.png",
     navItems: [
       { id: "home", label: "Home" },
       { id: "services", label: "Services" },
